0d17f03a7f37d16523b639d24f9d244af47e6663
tunjiNg01 <nahtlosng@gmail.com>
2023-09-22 19:19:47 +0100

    Functions in kotlin

--- core/src/main/kotlin/Main.kt
+++ core/src/main/kotlin/Main.kt
@@ -39,15 +39,79 @@ fun main(args: Array<String>) {
 
     juiceMenu.remove("orange")
     println(juiceMenu.containsKey("apple"))
-    println(juiceMenu.keys)
-    println(juiceMenu.values)
+   // println(juiceMenu.keys)
+   // println(juiceMenu.values)
 
-    println("orange" in readOnlyJuiceMenu.keys)
+   // println("orange" in readOnlyJuiceMenu.keys)
 // true
-    println(200 in readOnlyJuiceMenu.values)
+  //  println(200 in readOnlyJuiceMenu.values)
 // false
 
+    // Control flow in kotlin
+
+    val name = "1"
+
+   val result = when(name) {
+        "Tunji" -> {
+            println(name)
+        }
+        "1" -> println(1)
+        else -> {
+            println("unknown object")
+        }
+    }
+
+    val temp = 18
+
+    val description = when {
+        // If temp < 0 is true, sets description to "very cold"
+        temp < 0 -> "very cold"
+        // If temp < 10 is true, sets description to "a bit cold"
+        temp < 10 -> "a bit cold"
+        // If temp < 20 is true, sets description to "warm"
+        temp < 20 -> "warm"
+        // Sets description to "hot" if no previous condition is satisfied
+        else -> "hot"
+    }
+  //  println(description)
    // println("This map has ${readOnlyJuiceMenu.count()} key-value pairs")
+
+    for (i in 5 downTo 1 step 2){
+     //   println(i)
+    }
+
+    for (j in 'a'..'d') {
+      //  println(j)
+    }
+
+    var cakesEaten = 0
+    while (cakesEaten < 3) {
+        // println("Eat a cake")
+        cakesEaten++
+    }
+
+    // functions
+    fun greet() {
+        println("Good morning Tunji")
+    }
+
+    greet()
+    fun sum(x: Int, y: Int) : Int {
+        println("[$x] is: ...")
+       return  (x + y)
+    }
+
+    // println(sum(23,45))
+
+    fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+        println("[$prefix] $message")
+    }
+
+    printMessageWithPrefix(prefix = "Log", message = "Logged in to account" )
+   // single expression function
+    fun singleExp(x: Int, y: Int) = x + y
+    // println(singleExp(y = 23, x = 28))
+
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
